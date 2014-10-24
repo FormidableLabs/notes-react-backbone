6e697d7a8d1a431705125efb9a1d9b2f49b7cc53
Ryan Roemer <ryan@loose-bits.com>
2014-10-23 17:38:23 -0700

    Start adding event handler TODOs.

--- app/js/app/views/notes-item.jsx
+++ app/js/app/views/notes-item.jsx
@@ -8,20 +8,32 @@
 var React = require("react");
 
 module.exports = React.createClass({
+  // TODO: REMOVE.
+  TODO: function (key) {
+    var self = this;
+    return function () {
+      window.console.log("TODO " + key, self.props.note);
+    };
+  },
+
   render: function () {
     return (/*jshint ignore:start */
-      <tr className="notes-item">
+      <tr id={this.props.note.id}
+          className="notes-item">
         <td className="note-name">
-          <div className="note-title note-view">
+          <div className="note-title note-view"
+               onClick={this.TODO("VIEW")}>
             {this.props.note.get("title")}
           </div>
         </td>
         <td className="note-action">
           <div className="btn-group btn-group-sm pull-right">
-            <button className="btn btn-default note-edit">
+            <button className="btn btn-default note-edit"
+                    onClick={this.TODO("EDIT")}>
               <span className="glyphicon glyphicon-pencil"></span>
             </button>
-            <button className="btn btn-default note-delete">
+            <button className="btn btn-default note-delete"
+                    onClick={this.TODO("DELETE")}>
               <span className="glyphicon glyphicon-trash"></span>
             </button>
           </div>
