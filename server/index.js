8e046a5a51b446194c060503a16c533eab4ebffc
Ryan Roemer <ryan@loose-bits.com>
2014-11-01 09:14:44 -0700

    Refactor the API and express server setup.

--- server/index.js
+++ server/index.js
@@ -1,60 +1,69 @@
-var express = require("express"),
-  app = express(),
-  bodyParser = require("body-parser"),
-  sql = require("sqlite3"),
-  dbPath = __dirname + "/notes.sqlite",
-  db = null,
-  PORT = process.env.PORT || 3000;
+var express = require("express");
+var bodyParser = require("body-parser");
+var sql = require("sqlite3");
 
-function serverSetup() {
-  app.use("/js-dist/*.map", function (req, res) {
-    res.send(404, "404"); // Prevent sourcemap serving.
-  });
-  app.use("/", express["static"]("app"));
-  app.use("/app", express["static"]("app"));
-  app.use("/test", express["static"]("test"));
-  app.use("/node_modules", express["static"]("node_modules"));
-  app.use(bodyParser());
+var app = express();
+var db = null;
 
-  app.get("/notes", function (req, res) {
-    db.prepare("select * from notes")
-      .all(function (err, rows) {
-        res.json(rows);
-      });
-  });
+var DB_PATH = __dirname + "/notes.sqlite";
+var PORT = process.env.PORT || 3000;
 
-  // TODO: sanitize input
-  app.post("/notes", function (req, res) {
-    var title = req.body.title || "",
-      text = req.body.text || "";
-    db.run("insert into notes (title, text) values(?,?)", title, text)
-      .prepare("select * from notes order by id desc limit 1")
-      .get(function (err, row) {
-        res.json(row);
-      });
-  });
+// ----------------------------------------------------------------------------
+// Setup, Static Routes
+// ----------------------------------------------------------------------------
+app.use("/js-dist/*.map", function (req, res) {
+  res.send(404, "404"); // Prevent sourcemap serving.
+});
+app.use("/", express["static"]("app"));
+app.use("/app", express["static"]("app"));
+app.use("/test", express["static"]("test"));
+app.use("/node_modules", express["static"]("node_modules"));
+app.use(bodyParser());
 
-  // TODO: sanitize input
-  app.put("/notes/:id", function (req, res) {
-    var title = req.body.title,
-      text = req.body.text,
-      id = req.params.id;
+// ----------------------------------------------------------------------------
+// API
+// ----------------------------------------------------------------------------
+var _errOrData = function (res, dataOverride) {
+  return function (err, data) {
+    if (err) {
+      res.status(500).json({ error: err.message || err.toString() });
+    }
 
-    db.run("update notes set title=?, text=? where id=?", title, text, id)
-      .prepare("select * from notes where id=?", id)
-      .get(function (err, row) {
-        res.json(row);
-      });
-  });
+    res.json(dataOverride || data);
+  };
+};
 
-  // TODO: sanitize input
-  app["delete"]("/notes/:id", function (req, res) {
-    db.run("delete from notes where id=?", req.params.id, function () {
-      res.json({});
-    });
-  });
+app.get("/notes", function (req, res) {
+  db.prepare("select * from notes")
+    .all(_errOrData(res));
+});
 
+app.post("/notes", function (req, res) {
+  var title = req.body.title || "",
+    text = req.body.text || "";
+
+  db.run("insert into notes (title, text) values(?,?)", title, text)
+    .prepare("select * from notes order by id desc limit 1")
+    .get(_errOrData(res));
+});
+
+app.put("/notes/:id", function (req, res) {
+  var title = req.body.title,
+    text = req.body.text,
+    id = req.params.id;
+
+  db.run("update notes set title=?, text=? where id=?", title, text, id)
+    .prepare("select * from notes where id=?", id)
+    .get(_errOrData(res));
+});
+
+app["delete"]("/notes/:id", function (req, res) {
+  db.run("delete from notes where id=?", req.params.id, _errOrData(res, {}));
+});
+
+// ----------------------------------------------------------------------------
+// Bootstrap
+// ----------------------------------------------------------------------------
+db = new sql.Database(DB_PATH, sql.OPEN_READWRITE, function () {
   app.listen(PORT);
-}
-
-db = new sql.Database(dbPath, sql.OPEN_READWRITE, serverSetup);
+});
